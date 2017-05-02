29a4427d8f8cc698cdfd5333f255dad655d08cea
phoebuscy <phoebuscy@163.com>
2017-05-03 00:30:26 +0800

    修改运行批处理，可以加载hotswap

--- code/build/run.bat
+++ code/build/run.bat
@@ -1,4 +1,15 @@
 
-set x=%~dp0%.\spy\spy.par\twsapi-spy.jar
-java -jar %x% %*
+set current_dir=%~dp0%
+set install_root=%current_dir%
+set java_home=%install_root%jdk1.8.0_131\
+set path=%java_home%jre\bin;%path%
+SET CLASSPATH=.;%java_home%lib\dt.jar;%java_home%lib\tools.jar
+set java="%java_home%bin\java.exe"
 
+
+set port=8788
+
+set jvm_opts=-Xdebug -Xrunjdwp:transport=dt_socket,address=%port%,server=y,suspend=n -XXaltjvm=dcevm -javaagent:C:\hotswap\hotswap-agent-1.1.0-SNAPSHOT.jar
+
+set x=%~dp0%spy\spy.par\twsapi-spy.jar
+java  -jar %jvm_opts%  %x% %*
